51bd8f908d2949a64299fd85431daa52eb0a62dd
cdrosales <cara10denise@hotmail.com>
2020-01-18 16:56:03 -0500

    refactor: created link to footer info

--- footer.php
+++ footer.php
@@ -5,12 +5,14 @@
 
 
 <footer>
+    <a href="https://www.instagram.com/ty.thetrainer">
     <img src="<?php echo get_template_directory_uri() . '/assets/smoke.png'; ?>" class="background-smoke" />
 
     <div class="footer-wrapper">
         <i class="fab fa-instagram"></i>
         <h2>ty.thetrainer</h2> 
     </div>
+    </a>
 
 </footer>
 
